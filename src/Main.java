6321a26af3a2c50a5e1507d047cbaf0cb6547592
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-12-26 17:08:11 +0300

    задача первая

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+//метод возвечения часла в квадрат
 
+        int a = 5;
+        int d = 2;
+        System.out.println("Число 5 в квадрате равно " + Math.pow(a, d));
     }
 }
